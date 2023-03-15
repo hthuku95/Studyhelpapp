a4f9f33208395530bdb4cae5ad1ca6fa983041b1
HARRY OMOLLOH <hthuku95@gmail.com>
2023-03-15 13:21:00 +0300

    Db changes

--- studyhelp/settings.py
+++ studyhelp/settings.py
@@ -17,7 +17,7 @@ environ.Env.read_env()
 SECRET_KEY = 's@gc4pqkyjtgw2^^d&l%$s+w4f^9r^345r&qkjbtx9+8e9)%t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
