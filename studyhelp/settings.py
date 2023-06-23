7ebfebe1b96f0125630d8d2cfa9d0215a60b2830
HARRY OMOLLOH <hthuku95@gmail.com>
2023-06-23 15:42:10 +0300

    Added cache control

--- studyhelp/settings.py
+++ studyhelp/settings.py
@@ -162,6 +162,10 @@ AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
